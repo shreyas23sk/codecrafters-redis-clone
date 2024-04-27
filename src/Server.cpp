aa5aaf09d979002c2a567f884bd0216e2ee86da1
shreyas23sk <shreyas23.sk@gmail.com>
2024-04-28 03:16:04 +0530

    command process attempt 3

--- src/Server.cpp
+++ src/Server.cpp
@@ -359,6 +359,7 @@ int main(int argc, char **argv)
     recv(replica_fd, buf, sizeof(buf), 0);
     memset(buf, 0, 1024);
 
+    handle_client(replica_fd);
     //threads.emplace_back(std::thread(handle_client, replica_fd));
   }
 
